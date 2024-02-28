97b1167160761af49b0f4a2211137f94e696b16b
YoujunZhang <144183143+YoujunZhang@users.noreply.github.com>
2024-02-27 23:33:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function myFunction(element, clr) {
+    element.style.color = clr;
+}
+
+
+// new cursoreffects.fairyDustCursor({
+//     colors: ["#FFFEB9", "#FFFFE1", "#FFBE0F"],
+//   });
+
+
+
+
+function myFunction1() {
+    location.replace("http://127.0.0.1:5501/index.html")
+  }
+
+
+//   window.addEventListener("load", (event) => {
+//     new cursoreffects.followingDotCursor({ color: ["rgb(46, 46, 255)"] });
+//   });
+
+
+
+  
